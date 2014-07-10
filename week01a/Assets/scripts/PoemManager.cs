de2f739e76ea923148e164aaa515aec01f5a110c
Robert Yang <yang.robert.w@gmail.com>
2014-07-10 17:50:05 -0400

    week 1 -- added comments

--- week01a/Assets/scripts/PoemManager.cs
+++ week01a/Assets/scripts/PoemManager.cs
@@ -5,14 +5,11 @@ public class PoemManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		// spits out "Hello World" to Unity's console tab
 		Debug.Log("Hello World");
-		
+		// changes the text on the text mesh to say ____.
 		GetComponent<TextMesh>().text = "Bonjour Monde\nBienvenidos\nBlah";
+		// remember, type "\n" to denote a line break, like above!
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
