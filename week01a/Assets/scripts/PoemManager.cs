1897cb534261f52671e13c207849ec7dd7d67b58
Robert Yang <yang.robert.w@gmail.com>
2014-07-10 17:48:49 -0400

    added week 1 stuff (landscapes, terrain, intro to code)

---
+++ week01a/Assets/scripts/PoemManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Debug.Log("Hello World");
+		
+		GetComponent<TextMesh>().text = "Bonjour Monde\nBienvenidos\nBlah";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
