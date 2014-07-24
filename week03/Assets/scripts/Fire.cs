bed74cfdbaed29054e4c2e3e59a6fc94139152ac
Robert Yang <yang.robert.w@gmail.com>
2014-07-24 15:46:13 -0400

    week 3 kardashian mode!!!!

---
+++ week03/Assets/scripts/Fire.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	bool isKimHere = false;
+	public Transform ladder;
+
+	void OnTriggerEnter ( Collider bradPitt ) {
+		//Destroy ( bradPitt.gameObject );
+		isKimHere = true;
+		
+	}
+	
+	void Update () {
+		if (isKimHere == true && Input.GetKeyDown (KeyCode.Space)) {
+			// open Kim's car odor
+			ladder.position = Vector3.zero;
+		}
+	}
+}
